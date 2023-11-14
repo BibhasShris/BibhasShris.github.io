1e508963b95ec980d2baeeccd60e65c60a79ac69
BibhasShris <bish4421@colorado.edu>
2023-11-14 13:33:02 -0700

    new up

--- ica/ica12/ica12.js
+++ ica/ica12/ica12.js
@@ -1,8 +1,12 @@
 const newBtn = document.querySelector(`#js-new-quote`);
-const answerBtn = document.querySelector(`#js-answer-text`);
+const answerBtn = document.querySelector(`#js-tweet`);
 newBtn.addEventListener("click", getQuote);
-answerBtn.addEventListener(`click`, getAnswer);
+answerBtn.addEventListener(`click`, displayAnswer);
 
+const questionTxt = document.querySelector('#js-quote-text');
+let answerTxt = document.querySelector('#js-answer-text');
+
+// this is the endpoint for the API that we want to get a reponse from
 const endpoint = `https://trivia.cyberwisp.com/getrandomchristmasquestion`;
 
 let answer = '';
@@ -10,7 +14,7 @@ let answer = '';
 async function getQuote()
 {
     // console.log(`test`);
-
+ // try -> tries something; if it returns an error, it puts us into the catch block
     try{
         const response = await fetch(endpoint);
         if(!response.ok)
@@ -19,8 +23,11 @@ async function getQuote()
         }
         const json = await response.json();
         // console.log(json);
+
+        // JSON is a dictionary, which is like a list; we call specific pieces of information out based on the 'key' associated with that value
         displayQuote(json[`question`]);
         answer = json[`answer`];
+        answerTxt.textContent = '';
 
     }
     catch(err){
@@ -29,15 +36,17 @@ async function getQuote()
     }
 }
 
+// this function shows the question
 function displayQuote(question){
-    const questionTxt=document.querySelector(`#js-quote-text`);
     questionTxt.textContent = question;
 }
 
-function displayAnswer(answer)
+// this function shows the answer
+function displayAnswer()
 {
-    const answerTxt = document.querySelector(`#js-answer-text`);
-    answerTxt.textContent = ``;
+    answerTxt.textContent = answer;
 }
 
+// we run getQuote once when the script first loads to populate a question
+// when the page is loading
 getQuote();
